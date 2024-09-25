12fc3968b48060ccd7a05637becf688ff2253cc9
culaido2018 <culaido@owlting.com>
2024-09-25 12:10:22 +0800

    init

--- pages/about.vue
+++ pages/about.vue
@@ -11,12 +11,6 @@ useHead({
   ],
 })
 
-defineOgImageComponent('About', {
-  headline: 'Greetings 👋',
-  title: navbarData.homeTitle,
-  description: 'Dive into web development with me and learn Js, Ts, Vue, Nuxt, Docker, k8s',
-  link: '/riyad.jpg',
-})
 </script>
 
 <template>
@@ -65,13 +59,7 @@ defineOgImageComponent('About', {
             </div>
           </div>
           <div class="sm:hidden block col-span-3 pb-5 dark:text-[#F1F2F4]">
-            <NuxtImg
-              src="/riyad.jpg"
-              width="125"
-              height="115"
-              quality="50"
-              class="rounded-md"
-            />
+
           </div>
         </div>
         <h3 class="text-base sm:text-3xl font-semibold pb-7 sm:pb-12">
@@ -81,13 +69,7 @@ defineOgImageComponent('About', {
         <p>{{ aboutPage.aboutMe }}</p>
       </div>
       <div class="hidden sm:block col-span-3">
-        <NuxtImg
-          src="/riyad.jpg"
-          width="450"
-          height="500"
-          quality="50"
-          class="rounded-md"
-        />
+
       </div>
     </div>
   </div>
